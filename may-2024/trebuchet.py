157404ba2a86703870949a5356fbbf48eae6d5a6
Dr Pawel Orzechowski <24914654+drpawelo@users.noreply.github.com>
2024-05-03 10:51:28 +0100

    I wrote get_first_digit function and are_you_a_digit

--- may-2024/trebuchet.py
+++ may-2024/trebuchet.py
@@ -1,17 +1,30 @@
 input_strings = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]
+digits = ["0","1","2","3","4","5","6","7","8","9"]
+
+def are_you_a_digit(some_character):
+    if some_character in digits:
+           return True
+    else: 
+           return False
+
+# print(are_you_a_digit("z")) #False
+# print(are_you_a_digit("7")) #True
+# print(are_you_a_digit("r")) #False
+# print(are_you_a_digit("0")) #True
+
+
 
 # given a string, return the first digit
 def get_first_digit(word):
-    first_digit = None  
-
     # loop round the word, are you a digit?
     for character in word:
-        print(character)
+        if are_you_a_digit(character) == True:
+                return character
 
-
-    return first_digit
-
-print(get_first_digit("1abc2"))
+print(get_first_digit("1abc2")) # 1
+print(get_first_digit("pqr3stu8vwx")) #3
+print(get_first_digit("a1b2c3d4e5f")) #1
+# assert get_first_digit("1abc2") == 1
 
 # given a string, return the last digit
 
