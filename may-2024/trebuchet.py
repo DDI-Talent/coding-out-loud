4bc91d097a10a04561e8ec70acf33943da7e01c6
Dr Pawel Orzechowski <24914654+drpawelo@users.noreply.github.com>
2024-05-03 11:11:30 +0100

    puzzle solution works! yay

--- may-2024/trebuchet.py
+++ may-2024/trebuchet.py
@@ -25,16 +25,19 @@ def get_last_digit(word):
 def get_first_and_last_digits_combined(word):
     return int(get_first_digit(word) + get_last_digit(word))
 
+
+input_strings = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]
+
 # sum all the numbers, return the sum
 def sum_of_numbers_in_words(list_of_words):
-    total = 0
+      list_of_numbers = [
+           get_first_and_last_digits_combined(word)
+			for word in list_of_words
+	  ]
+      print(list_of_numbers)
+      return sum(list_of_numbers)
 
-    # list comp
-
-    return total
-
-
-
+print(sum_of_numbers_in_words(input_strings))
 
 
 
