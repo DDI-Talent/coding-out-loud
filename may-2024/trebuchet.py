af70c668e25ea510749a6cae41b47562348724bb
Keith Douglas <keith@keithdouglas.co.uk>
2024-05-03 10:30:27 +0100

    get first digit scffolded out

---
+++ may-2024/trebuchet.py
@@ -0,0 +1,68 @@
+input_strings = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]
+
+# given a string, return the first digit
+def get_first_digit(word):
+    first_digit = None  
+
+    # loop round the word, are you a digit?
+    for character in word:
+        print(character)
+
+
+    return first_digit
+
+print(get_first_digit("1abc2"))
+
+# given a string, return the last digit
+
+# given a string, return first and last digits combined
+
+# string to number
+
+# sum all the numbers, reurn the sum
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# expected_output_numbers = [12,38,15,77]
