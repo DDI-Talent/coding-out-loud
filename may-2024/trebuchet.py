ce064329ce0e3eebd65ab7895e2a5f76dd58228d
Keith Douglas <keith@keithdouglas.co.uk>
2024-05-03 13:07:39 +0100

    adds reading file data

--- may-2024/trebuchet.py
+++ may-2024/trebuchet.py
@@ -1,5 +1,18 @@
+# test data
 input_strings = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]
 digits = ["0","1","2","3","4","5","6","7","8","9"]
+
+# load the file:
+
+def load_file_lines(filename):
+    lines = []
+    file = open(filename)
+    for line in file:
+        lines.append(line.rstrip("\n"))
+    file.close()
+    return lines
+
+datas = load_file_lines("./data/data.txt")
 
 def are_you_a_digit(some_character):
     if some_character in digits:
@@ -36,11 +49,7 @@ def get_first_and_last_digits_combined(word):
 	# "1" "5"
     # "15"
     # 15
-	# grab first and last and num them
-
-
-
-input_strings = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]
+	# grab first and last and "num" them
 
 # sum all the numbers, return the sum
 def sum_of_numbers_in_words(list_of_words):
@@ -48,62 +57,8 @@ def sum_of_numbers_in_words(list_of_words):
            get_first_and_last_digits_combined(word)
 			for word in list_of_words
 	  ]
-      print(list_of_numbers)
+     
       return sum(list_of_numbers)
 
-print(sum_of_numbers_in_words(input_strings))
-
-
-# load the file:
-
-def load_file_lines(filename):
-    lines = []
-    file = open(filename)
-    for line in file:
-        lines.append(line.rstrip("\n"))
-    file.close()
-    return lines
-
-datas = load_file_lines("./pawel.txt")
 print(sum_of_numbers_in_words(datas))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# expected_output_numbers = [12,38,15,77]
