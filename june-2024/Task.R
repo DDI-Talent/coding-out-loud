df108984d10b4294b22c8102a0d1793786ed1379
Kasia Banas <kasia.banas@gmail.com>
2024-06-07 13:17:52 +0100

    write out the steps

--- june-2024/Task.R
+++ june-2024/Task.R
@@ -1,3 +1,15 @@
 # Given a vector of birthdates, write a function to compute the age in years.
 
+# Step 1: Define a function name and an empty function
 
+# Step 2: Name the inputs (aka arguments or parameters)
+
+# Step 3: Define the return value
+
+# Step 4: Test the function
+
+# Step 5: Define a variable that will represent the input
+
+# Step 6: Test the function again
+
+# Step 7: Write the algorithm within the function
