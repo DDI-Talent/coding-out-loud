1cbb31053e6e92eac806bb5e8bedb1cab8166ed2
Keith Douglas <keith@keithdouglas.co.uk>
2024-07-05 13:29:30 +0100

    created project file for July

--- june-2024/actors_ages.r
+++ june-2024/actors_ages.r
@@ -1,13 +1,13 @@
 library(tidyverse)
 library(here)
-source(here('june-2024/Task.R'))
+source(here("june-2024/Task.R"))
 
-age_gaps <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-02-14/age_gaps.csv')
+age_gaps <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-02-14/age_gaps.csv') # nolint
 
-actor_1s <- age_gaps %>% 
+actor_1s <- age_gaps %>%
   select(name = actor_1_name, dob = actor_1_birthdate)
 
-actor_2s <- age_gaps %>% 
+actor_2s <- age_gaps %>%
   select(name = actor_2_name, dob = actor_2_birthdate)
 
 actor_ages <- bind_rows(actor_1s, actor_2s) %>% 
