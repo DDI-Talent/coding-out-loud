776b2229af8d56deb76b55b987c33ac20a0c4947
Keith Douglas <keith@keithdouglas.co.uk>
2024-08-09 12:28:59 +0100

    Final version

--- august-2024/First_ShinyApp/app.R
+++ august-2024/First_ShinyApp/app.R
@@ -1,82 +1,94 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-# UI, Server, ShinyApp (ui,server)
-
 library(shiny)
 
-# Define UI for application that draws a histogram
+# Define UI for random distribution app ----
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Tabsets"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-          radioButtons("dist", "Distripution Types:",
-                       c("Normal"="norm",
-                         "Uniform"="unif",
-                         "Log-normal"="lnorm",
-                         "Exponential"="exp")),
-          
-          sliderInput("n",
-                        "Number of observations",
-                        min = 1,
-                        max = 1000,
-                        value = 500)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-          tabsetPanel(type = "tabs",
-                      tabPanel("Plot",plotOutput("plot")),
-                      tabPanel("Summary",textOutput("summary"))
-           
-        )
+  
+  # App title ----
+  titlePanel("Tabsets"),
+  
+  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+    
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+      
+      # Input: Select the random distribution type ----
+      radioButtons("dist", "Distribution type:",
+                   c("Normal" = "norm",
+                     "Uniform" = "unif",
+                     "Log-normal" = "lnorm",
+                     "Exponential" = "exp")),
+      
+      # br() element to introduce extra vertical spacing ----
+      #br(),
+      
+      # Input: Slider for the number of observations to generate ----
+      sliderInput("n",
+                  "Number of observations:",
+                  value = 500,
+                  min = 1,
+                  max = 1000)
+      
+    ),
+    
+    # Main panel for displaying outputs ----
+    mainPanel(
+      
+      # Output: Tabset w/ plot, summary, and table ----
+      tabsetPanel(type = "tabs",
+                  tabPanel("Plot", plotOutput("plot")),
+                  tabPanel("Summary", verbatimTextOutput("summary")),
+                  tabPanel("Table", tableOutput("table"))
+      )
+      
     )
   )
 )
 
-# Define server logic required to draw a histogram
+# Define server logic for random distribution app ----
 server <- function(input, output) {
- d <-reactive({
-   dist<-switch(input$dist,
-                norm=rnorm,
-                unif=runif,
-                lnorm=rlnorm,
-                exp=rexp,
-                rnorm)
-   dist(input$n)
- }) 
-
   
-    output$plot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-
-        # draw the histogram with the specified number of bins
-        hist(d(), col = 'darkgray', border = 'white',
-             xlab = 'number',
-             main = 'Distribution')
-    })
+  # Reactive expression to generate the requested distribution ----
+  # This is called whenever the inputs change. The output functions
+  # defined below then use the value computed from this expression
+  d <- reactive({
+    dist <- switch(input$dist,
+                   norm = rnorm,
+                   unif = runif,
+                   lnorm = rlnorm,
+                   exp = rexp,
+                   rnorm)
     
-    output$summary <- renderPrint(summary(d()))
+    dist(input$n)
+  })
+  
+  # Generate a plot of the data ----
+  # Also uses the inputs to build the plot label. Note that the
+  # dependencies on the inputs and the data reactive expression are
+  # both tracked, and all expressions are called in the sequence
+  # implied by the dependency graph.
+  output$plot <- renderPlot({
+    #dist <- input$dist
+    # n <- input$n
+    #hist(d(),
+    # main = paste("r", dist, "(", n, ")", sep = ""),
+    #col = "#75AADB", border = "white")
+    hist(d(),
+         main = paste("r", input$dist, "(", input$n, ")", sep = ""),
+         col = "#75AADB", border = "white")
+  })
+  
+  # Generate a summary of the data ----
+  output$summary <- renderPrint({
+    summary(d())
+  })
+  
+  # Generate an HTML table view of the data ----
+  output$table <- renderTable({
+    d()
+  })
+  
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
-
-
-
-
-
-
-
-
-
-
+# Create Shiny app ----
+shinyApp(ui, server)
