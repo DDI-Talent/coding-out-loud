c9aca13718a520a2713168c0eb7d7c12a4bc1905
Keith <>
2024-09-05 14:19:19 +0000

    adds all_words list

--- september-2024/wordle/game_engine.py
+++ september-2024/wordle/game_engine.py
@@ -3,8 +3,12 @@ class Game:
     def __init__(self):
         self.secret = "lucky"
         self.guesses = []
+        self.all_words = ["apple", "grape", "plane", "spark", "light", "stone", "bread", "cloud", "flame", "water"]
+
     
     def show_secret(self):
         return self.secret
+    
+        
     
     
