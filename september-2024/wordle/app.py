8ab78b42e58c9590c2dd47a9227447b14474c25e
Keith <>
2024-09-05 10:53:27 +0000

    initial commit

---
+++ september-2024/wordle/app.py
@@ -0,0 +1,6 @@
+from game_engine import Game
+
+
+my_game = Game()
+
+print(my_game.show_secret())
---
+++ september-2024/wordle/game_engine.py
@@ -0,0 +1,10 @@
+class Game:
+    
+    def __init__(self):
+        self.secret = "lucky"
+        self.guesses = []
+    
+    def show_secret(self):
+        return self.secret
+    
+    
