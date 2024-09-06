c9676eaa0a345101b3d5b6ba11d09891aeab5515
banana <banana@ed.ac.uk>
2024-09-06 10:56:16 +0000

    feedback now shows to user

--- september-2024/wordle/game_engine.py
+++ september-2024/wordle/game_engine.py
@@ -11,7 +11,8 @@ class Game:
         self.all_words = self.load_basic_english_words()
 
     def reset_game(self):
-        self.secret = random.choice(self.all_words)
+        self.secret = "apple"
+        # random.choice(self.all_words)
         self.guesses = []
         # self.leftover_words = self.all_words
               
@@ -33,23 +34,35 @@ class Game:
         self.reset_game()
         
         
-        while len(self.guesses) < self.max_guesses:
+        while len(self.guesses) < self.max_guesses and not (self.secret in self.guesses):
             guess = input("What's your guess? ")
             self.guesses.append(guess)
             feedback = self.get_feedback(guess)
             print(feedback)
+            print(self.get_feedback_for_humans(guess))
 #  
     
-        
+    
+#    ("antil")  "apple" -> "+--?-"
     def get_feedback(self, guess):
         feedback = ""
-        print(self.secret)
-#         if it's the right word, return Win!
+        for index in range(0,5): #[0,1,2,3,4]
+            if guess[index] == self.secret[index]:
+                feedback += "+"
+            elif guess[index] in self.secret: #and guess[index] != self.secret[index] 
+                feedback += "?"
+            else:
+                feedback += "-"
+        return feedback
         
-        if guess == self.secret:
-            return "Win!"
+        
+
+    def get_feedback_for_humans(self, guess):
+        feedback = self.get_feedback(guess)
+        if feedback == "+++++":
+            return f"You Win! Secret was {self.secret}, you guessed in {len(self.guesses)}"
         else:
-            return "Try again"
+            return f"You guessed   {guess}\nyour feedback {feedback}"
     
 
 
