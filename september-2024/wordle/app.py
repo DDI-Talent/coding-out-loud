dde431199d45d19848b87c7e825b48062ccaa721
banana <banana@ed.ac.uk>
2024-09-06 11:16:23 +0000

    autoplay

--- september-2024/wordle/app.py
+++ september-2024/wordle/app.py
@@ -1,4 +1,6 @@
 # file that will be run in terminal using "python app.py"
 from game_engine import Game 
 my_game = Game()
-my_game.start()
+# my_game.start()
+my_game.auto_play()
+
--- september-2024/wordle/game_engine.py
+++ september-2024/wordle/game_engine.py
@@ -14,7 +14,37 @@ class Game:
         self.secret = random.choice(self.all_words)
         self.guesses = []
         self.leftover_words = self.all_words
-              
+    
+    
+    def words_that_fit_feedback(self,guess):
+#         apple
+#         ++?--
+        feedback = self.get_feedback(guess)
+
+        allowed_words_for_feedbacks = []
+
+        for index in range(0,5):
+            words_that_meet_feedback_item = [] #eg 'A' is at index 0
+            if feedback[index] == "+": # letter is there!
+    #               keep words where letter A is at index 0
+                for word in self.leftover_words:
+                    if word[index] == guess[index]: 
+                        words_that_meet_feedback_item.append(word)
+            elif feedback[index] == "-": # letter not there
+    #               keep words where letter A is not there at all
+                for word in self.leftover_words:
+                    if not guess[index] in word: 
+                        words_that_meet_feedback_item.append(word)
+            elif feedback[index] == "?": # letter there, but elsewhere
+                for word in self.leftover_words:
+                    if guess[index] in word and word[index] != guess[index]: 
+                        words_that_meet_feedback_item.append(word) 
+
+            allowed_words_for_feedbacks.append(words_that_meet_feedback_item)
+
+    #     overlap
+        self.leftover_words  = set(allowed_words_for_feedbacks[0]) & set(allowed_words_for_feedbacks[1]) & set(allowed_words_for_feedbacks[2]) & set(allowed_words_for_feedbacks[3]) & set(allowed_words_for_feedbacks[4]  )
+
     def load_basic_english_words(self):
         with open("xkcd_simple_words.csv") as file:
             reader = csv.reader(file, delimiter=",", quotechar='"')
@@ -29,6 +59,23 @@ class Game:
             
             return words_to_keep
     
+        
+    def auto_play(self):
+        scores = []
+        for which_go in range(10000):
+            self.reset_game()
+            while len(self.guesses) < self.max_guesses and not (self.secret in self.guesses):
+                guess = random.choice( list(self.leftover_words) )
+                self.guesses.append(guess)
+                self.words_that_fit_feedback(guess)
+                feedback = self.get_feedback(guess)
+                # print(feedback)
+                # print(self.get_feedback_for_humans(guess))
+            scores.append(len(self.guesses))
+        print(f"on average you win every { sum(scores) / len(scores)}")
+#  
+    
+    
     def start(self):
         self.reset_game()
         
@@ -36,6 +83,7 @@ class Game:
         while len(self.guesses) < self.max_guesses and not (self.secret in self.guesses):
             guess = input("What's your guess? ")
             self.guesses.append(guess)
+            self.words_that_fit_feedback(guess)
             feedback = self.get_feedback(guess)
             print(feedback)
             print(self.get_feedback_for_humans(guess))
@@ -61,7 +109,7 @@ class Game:
         if feedback == "+++++":
             return f"You Win! Secret was {self.secret}, you guessed in {len(self.guesses)}"
         else:
-            return f"You guessed   {guess}\nyour feedback {feedback}"
+            return f"You guessed   {guess}\nyour feedback {feedback}\nleftover words are {self.leftover_words}"
     
 
 
