d040d7b2208753eb8c1715bf4216dbaf2ea43e8c
Keith Douglas <keith@keithdouglas.co.uk>
2024-12-02 11:49:53 +0000

    adds snow covered ground

--- christmas-tree-SOLUTION.R
+++ christmas-tree-SOLUTION.R
@@ -3,20 +3,20 @@ library(ggplot2)
 
 # Create the tree data (three triangles for the tree and one rectangle for the trunk)
 tree_data <- data.frame(
-  x = c(1.5, 2.5, 2, # Triangle 1 vertices
-        1.5, 2.5, 2, # Triangle 2 vertices
-        1.7, 2.3, 2, # Triangle 3 vertices
-        1.9, 1.9, 2.1, 2.1 # Trunk vertices
+  x = c(2.5, 3, 2.75, # Triangle 1 
+        2.5, 3, 2.75, # Triangle 2 
+        2.5, 3, 2.75, # Triangle 3 
+        0, 0, 4, 4 # Ground 
       ),
-  y = c(0, 0, 1,       # Triangle 1 vertices
-        0.5, 0.5, 1.5, # Triangle 2 vertices
-        1.2, 1.2, 2, # Triangle 3 vertices
-        -0.3, 0, 0, -0.3 # Trunk vertices
+  y = c(0, 0, 1,       # Triangle 1 
+        0.5, 0.5, 1.5, # Triangle 2 
+        1.2, 1.2, 2, # Triangle 3 
+        0, 0.3, 0.3, 0 # Ground 
         ),
   group = c(1, 1, 1, # Group for Triangle 1
             2, 2, 2, # Group for Triangle 2
             3, 3, 3,  # Group for Triangle 3
-            4, 4, 4, 4  # Group for Triangle 3
+            4, 4, 4, 4  # Group for Ground
             )  
 )
 
@@ -24,10 +24,12 @@ tree_data <- data.frame(
 # Plot the triangle
 ggplot(tree_data, aes(x = x, y = y, group = group)) +
   geom_polygon(aes(fill = factor(group)), color = "black") +  # Fill and outline color
-  scale_fill_manual(values = c("forestgreen", "darkgreen", "forestgreen", "brown4")) +
-  coord_fixed() + # Ensures the x and y axes are scaled equally, keeping the triangle proportions correct.
+  scale_fill_manual(values = c("forestgreen", "darkgreen", "forestgreen", "grey98")) +
   theme_void() +
   theme(
-    legend.position = "none"
+    legend.position = "none",
+    plot.background = element_rect(
+      fill = "midnightblue"
+    )
   )
 
