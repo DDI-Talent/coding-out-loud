d8945409c7abb1d4e22299c3e04329229450f076
Keith Douglas <keith@keithdouglas.co.uk>
2024-12-03 11:12:44 +0000

    renames triangles to sections

--- december-2024/christmas-tree-SOLUTION.R
+++ december-2024/christmas-tree-SOLUTION.R
@@ -1,21 +1,21 @@
 # Load ggplot2
 library(ggplot2)
 
-# Create the tree data (three triangles for the tree and one rectangle for the trunk)
+# Create the tree data (three triangles for the tree and one rectangle for the ground)
 tree_data <- data.frame(
-  x = c(1.5, 3, 2, # Triangle 1 
-        1.5, 3, 2, # Triangle 2 
-        1.5, 3, 2, # Top 
+  x = c(1.5, 2.5, 2, # Bottom 
+        1.5, 2.5, 2, # Middle
+        1.5, 2.5, 2, # Top 
         0, 0, 3, 3 # Ground 
       ),
-  y = c(0, 0, 1,       # Triangle 1 
-        0.5, 0.5, 1.5, # Triangle 2 
+  y = c(0, 0, 1,       # Bottom 
+        0.5, 0.5, 1.5, # Middle
         1.1, 1.1, 2, # Top 
         0, 0.2, 0.2, 0 # Ground 
         ),
-  group = c(1, 1, 1, # Group for Triangle 1
-            2, 2, 2, # Group for Triangle 2
-            3, 3, 3,  # Group for Triangle 3
+  group = c(1, 1, 1, # Bottom
+            2, 2, 2, # Middle
+            3, 3, 3,  # Top
             4, 4, 4, 4  # Group for Ground
             )  
 )
@@ -34,8 +34,12 @@ ggplot(tree_data, aes(x = x, y = y, group = group)) +
   geom_polygon(aes(fill = factor(group)), color = "black") +  # Fill and outline color
   scale_fill_manual(values = c("forestgreen", "darkgreen", "forestgreen", "grey98")) +
   
-  # Add a star at the top
+  # Add a star
   geom_point(aes(x = 0.5, y = 2.5), shape = 8, color = "yellow", size = 5, inherit.aes = FALSE) +
+  
+  # Add a bauble at the top
+  geom_point(aes(x = 2, y = 2), shape = 16, color = "tomato2", size = 5, inherit.aes = FALSE) +
+  
   
   geom_point(
     data = snowflakes,
@@ -53,9 +57,11 @@ ggplot(tree_data, aes(x = x, y = y, group = group)) +
     legend.position = "none",
     plot.background = element_rect(
       fill = "midnightblue"
-    )
+    ),
+    plot.title = element_text(color = "grey98", size = 16, hjust = 0.5, vjust = 0.5)
   ) +
   labs(
-    title = "Merry Christmas"
+    title = "Merry Christmas",
+    
   )
 
