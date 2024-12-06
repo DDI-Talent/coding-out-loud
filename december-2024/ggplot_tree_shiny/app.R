b1a20bee849d2d4fce9ffbfafd0cc22182512ece
banana <banana@ed.ac.uk>
2024-12-06 09:56:41 +0000

    shiny works now

--- december-2024/ggplot_tree_shiny/app.R
+++ december-2024/ggplot_tree_shiny/app.R
@@ -10,16 +10,11 @@ ui <- fluidPage(
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-      sliderInput("ornaments_number",
-                  "Number of ornaments:",
+      sliderInput("stars_number",
+                  "Number of stars:",
                   min = 1,
-                  max = 30,
-                  value = 10),
-      sliderInput("branches_number",
-                  "Number of branches:",
-                  min = 2,
-                  max = 10,
-                  value = 4)
+                  max = 300,
+                  value = 100)
     ),
     
     
@@ -35,64 +30,95 @@ server <- function(input, output) {
   
   output$distPlot <- renderPlot({
     
-    branches <-  input$branches_number
-    
-    
-    # input$branches_number
-    x_min <- -3
-    x_max <-  3
-    x_distance <- x_max - x_min
-    x_one_way_length_min <- 1
-    
-    y_min <- 1
-    y_max <- 4
-    y_distance <- y_max - y_min
-    y_one_height <- y_distance / (branches)
-    
-    
-    # tree
+
+    # Create the tree data (three triangles for the tree and one rectangle for the ground)
     tree_data <- data.frame(
-      x = c(rbind(rep(0, branches),
-                  seq( -x_one_way_length_min, x_min, by = (x_min + x_one_way_length_min) / (branches-1)),
-                  seq( x_one_way_length_min, x_max, by = (x_max - x_one_way_length_min) / (branches-1))
-      )),
-      y = c(rbind(seq(y_max,   y_min + y_one_height , by = - y_one_height),
-                  seq(y_max - y_one_height, y_min,   by = - y_one_height),
-                  seq(y_max - y_one_height, y_min,   by = - y_one_height)
-      )),
-      group = c(rep(seq(1,branches,1), each=3))
+      # left point, right point, top point
+      x = c(1.5, 2.5, 2, # Bottom 
+            1.5, 2.5, 2, # Middle
+            1.5, 2.5, 2, # Top 
+            0, 0, 3, 3 # Ground 
+      ),
+      y = c(0, 0, 1,       # Bottom 
+            0.5, 0.5, 1.5, # Middle
+            1.1, 1.1, 2, # Top 
+            0, 0.2, 0.2, 0 # Ground 
+      ),
+      group = c(1, 1, 1, # Bottom
+                2, 2, 2, # Middle
+                3, 3, 3,  # Top
+                4, 4, 4, 4  # Group for Ground
+      )  
     )
     
-    # tree_data <- data.frame(
-    #   x = c(0, -2, 2, 0, -3, 3, 0, -4, 4,  0, -5, 5),
-    #   y = c(4, 3, 3, 3, 2, 2, 2, 1, 1,1,0,0),
-    #   group = c(1, 1, 1, 2, 2, 2, 3, 3, 3,4,4,4)
-    # )
+    # https://realworlddatascience.net/ideas/tutorials/posts/2023/12/12/xmas-cards.html
+    set.seed(20241225)
+    n <- input$stars_number
     
     
-    print(tree_data)
-    
-    # trunk
-    trunk_data <- data.frame(
-      x = c(-0.5, 0.5, 0.5, -0.5),
-      y = c(1, 1, 0, 0)
-    )
-    
-    # bulbs
-    ornament_data <- data.frame(
-      x = round(runif(input$ornaments_number,-2.0, 3.0), 2),
-      y = round(runif(input$ornaments_number, 1.0, 3.5), 2)
+    # generates n random numbers drawn from a uniform distribution with a minimum value of 0 and a maximum value of 3
+    snowflakes <- data.frame(
+      x = runif(n, 0, 3),
+      y = runif(n, 0, 3)
     )
     
     
-    ggplot() +
-      geom_polygon(data = tree_data, aes(x = x, y = y, group = group), fill = "forestgreen", color = "darkgreen") +
-      geom_polygon(data = trunk_data, aes(x = x, y = y), fill = "brown", color = "black") +
-      geom_point(data = ornament_data, aes(x = x, y = y), color = "red", size = 10, alpha=I(0.7) ) +
-      theme_void() + # no axis
-      coord_fixed() + # size
-      ggtitle("Tree 🎄")
+    # Plot the triangles
+    ggplot(tree_data, aes(x = x, y = y, group = group)) +
+      
+      geom_polygon(aes(fill = factor(group)), color = "black") +  # Fill and outline color
+      scale_fill_manual(values = c("forestgreen", "darkgreen", "forestgreen", "grey98")) +
+      
+      # Add a star
+      geom_point(data = data.frame(
+        x = 1,
+        y = 2.5
+      ), 
+      aes(x = x, y = y), 
+      shape = 8, 
+      color = "yellow", 
+      size = 5,
+      inherit.aes = FALSE) +
+      
+      
+      # Add a bauble at the top
+      geom_point(data = data.frame(
+        x = 2,
+        y = 2
+      ),
+      aes(x = x, y = y), 
+      shape = 16, 
+      color = "tomato2", 
+      size = 5,
+      inherit.aes = FALSE) +
+      
+      # Add snowflakes
+      geom_point(
+        data = snowflakes,
+        mapping = aes(
+          x = x,
+          y = y
+        ),
+        colour = "grey98",
+        size = 0.6,
+        pch = 8,
+        inherit.aes = FALSE
+      ) +
+      # 
+      theme_void() +
+      theme(
+        legend.position = "none",
+        plot.background = element_rect(
+          fill = "midnightblue"
+        ),
+        plot.title = element_text(color = "yellow", size = 32, hjust = 0.5, vjust = -1, family = "serif")
+      ) +
+      labs(
+        title = "Merry Christmas"
+      )
     
+    
+    # -- end of plot
   })
 }
 
