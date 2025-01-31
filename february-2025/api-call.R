6bca44ea69749ff9f4d1f684099b35051ec0a441
Keith Douglas <keith@keithdouglas.co.uk>
2025-01-31 12:35:53 +0000

    adds initial api call

--- february-2025/api-call.R
+++ february-2025/api-call.R
@@ -17,7 +17,91 @@ resp_users <- req_perform(req_users)
 
 users_df <- resp_body_json(resp_users, simplifyVector = TRUE)$users
 
-users_df |>
-  select(password, username) |>
-  arrange(username) |>
-  slice(1:10)
+#####################################################################
+
+# Calculate the average age of users
+average_age <- users_df |>
+  summarize(avg_age = mean(age, na.rm = TRUE))
+
+print(average_age)
+
+#####################################################################
+
+# Count the number of users from each blood group
+users_by_bloodgroup <- users_df |> 
+  group_by(bloodGroup) |> 
+  summarize(count = n()) |> 
+  arrange(desc(count))
+
+print(users_by_bloodgroup)
+
+#####################################################################
+
+# Count the frequency of first names
+common_first_names <- users_df |> 
+  group_by(firstName) |> 
+  summarize(count = n()) |> 
+  arrange(desc(count))
+
+print(common_first_names)
+
+#####################################################################
+
+# Filter users residing in New York
+users_in_ny <- users_df |> 
+  filter(address$city == "New York")
+
+print(users_in_ny$firstName)
+
+#####################################################################
+
+# Filter users whose usernames start with 'A'
+users_username_A <- users_df |> 
+  filter(startsWith(username, "a"))
+
+print(users_username_A$firstName)
+
+#####################################################################
+
+library(ggplot2)
+
+# Plot histogram of user ages
+ggplot(users_df, aes(x = age)) +
+  geom_histogram(binwidth = 5, fill = "aquamarine", color = "black") +
+  labs(title = "Most users are in their late 20s or 30s", x = "Age", y = "Frequency")
+
+#####################################################################
+
+# Filter out users with missing email addresses (all of them have an email!)
+users_with_email <- users_df |> 
+  filter(!is.na(email))
+
+print(users_with_email$firstName)
+
+#####################################################################
+
+# Create age groups
+users_with_age_group <- users_df |> 
+  mutate(age_group = case_when(
+    age >= 18 & age <= 25 ~ "18-25",
+    age >= 26 & age <= 35 ~ "26-35",
+    age >= 36 & age <= 45 ~ "36-45",
+    age >= 46 & age <= 60 ~ "46-60",
+    age > 60 ~ "60+",
+    TRUE ~ "Unknown"
+  ), .after = birthDate)
+
+
+#####################################################################
+
+# Create a full name column
+users_with_fullname <- users_df |> 
+  mutate(full_name = paste(firstName, lastName), .after = lastName)
+
+#####################################################################
+
+users_birth_date_as_date <- users_df |> 
+  mutate(
+    birthDate = as.Date(birthDate, format = "%Y-%m-%d")
+  )
+
